b29fc19d8241bbe2dea9762e9c3e4cedd1335174
thomasindarto84 <82036145+thomasindarto84@users.noreply.github.com>
2021-08-30 13:42:42 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>SISTEM PENGAJUAN JUDUL SKRIPSI ONLINE</h1>
+<link rel="stylesheet" type"text/css" href="css/style.css">
+<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+
+</head>
+<body id="bg-login">
+    <div class="box-login">
+        <h2>login</h2>
+        <form_action="" method="POST">
+            <input type="text" name="user" placeholder="Username" class=" input-control">
+            <input type="password" name="pass" placeholder="Password" class=" input-control">
+            <input type="submit" name="submit" value="Login"class= "btn">
+        </form>
+<body style="background-color:powderblue;">
+
+</body>
+</html>
